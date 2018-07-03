6741a661a3e6391323bbfaf3c47ac7873f040a3e
M7MD999 <40772560+M7MD999@users.noreply.github.com>
2018-07-03 20:57:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.'
+const prefix = 'M'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -44,89 +44,4 @@ client.user.setStatus("dnd");
 client.on('ready', () => {                           
 client.user.setGame(``,'https://www.twitch.tv/tarikrs');                                                                                                                                                                                                                                                                                                                                                                                                                             client.user.setGame(`Mhelp | By XMGX`,'https://www.twitch.tv/v5bz');
 });  
-
-client.on('message', message => {
-                                if(!message.channel.guild) return;
-                        if (message.content.startsWith('Mping')) {
-                            if(!message.channel.guild) return;
-                            var msg = `${Date.now() - message.createdTimestamp}`
-                            var api = `${Math.round(client.ping)}`
-                            if (message.author.bot) return;
-                        let embed = new Discord.RichEmbed()
-                          .setFooter(message.author.username , message.author.avatarURL)
-                         .setColor("RANDOM")
-                         .setTimestamp()
-                         .addField('**Time Taken:**',msg + " ms :signal_strength: ")
-                        .addField('**WebSocket:**',api + " ms :signal_strength: ")
-         message.channel.send({embed:embed});
-                        }
-                    });
-
-client.on('message', message => {
-    if (message.content.startsWith("Mavatar")) {
-        var mentionned = message.mentions.users.first();
-    var x5bzm;
-      if(mentionned){
-          var x5bzm = mentionned;
-      } else {
-          var Hamada = message.author;
-          
-      }
-        const embed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setImage(`${Hamada.avatarURL}`)
-      message.channel.sendEmbed(embed);
-    }
-});
-
-client.on('message', message => {
-     if (message.author.bot) return;
-       if (message.content ===  "Mhelp") {
- message.channel.send('**Done Enter Command In Box :D**')
-message.author.sendMessage(`
-__________________________________________________
-Thx for use this bot 
-
-commands :
-
-
-Mping =to know your ping
-
-Mavatar =to see your avatar
-
-soom more comands
-________________________________________________
-   `);
-       }
-});
-
-
-
-
-
-client.on('message' , async (message) => {
-       if(message.content.startsWith(`<@${client.user.id}>`)) {
-              
- let responses = [
-        'do you need any help',
-        'hi',
-        ':rage:',
-        'what you want',
-        'hello',
-        'how are you',
-        '?',
-        'try Mhelp'
-    ]
-    
-    // Fetch a random item from the array
-    let fetched = responses[Math.floor(Math.random() * responses.length)];
-   message.reply(fetched)
-       }
-  
-});
-
-
-
-
-
 

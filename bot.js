f76611186e92267500771c218d9a68ff37d4cc05
M7MD999 <40772560+M7MD999@users.noreply.github.com>
2018-07-23 00:18:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -80,6 +80,8 @@ Mhack
 لخداع صديقك انك هكرته
 Mمريم
 لعبة مريم
+Mminecraft
+أسئلة ماين كرافتية
 Mعقاب
 لعبة عقاب
 ╚[❖══════❖]╝
@@ -507,6 +509,40 @@ const zead = [
   }
 });
 
+client.on('message', message => {
+if (Mpoints[message.author.id]) points[message.author.id] = {
+    points: 50,
+  };
+if (message.content.startsWith(prefix + 'minecraft')) { 
+    if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
+
+const type = require('./minecraft/minecraft.json'); 
+const item = type[Math.floor(Math.random() * type.length)]; 
+const filter = response => { 
+    return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
+};
+message.channel.send('**عندك 15 ثانية الحق**').then(msg => {
+    let embed = new Discord.RichEmbed()
+    .setColor('#00ff47')
+    .setFooter("ماينكرفت  | M Games", 'https://cdn.discordapp.com/attachments/465137484427296768/470700189443948544/379a371a61f3fdb334df01b2fbf89f7d298b3e7c_hq.png')
+    .setDescription(`** ${item.type}**`)
+
+    msg.channel.sendEmbed(embed).then(() => {
+        message.channel.awaitMessages(filter, { maxMatches: 1, time: 15000, errors: ['time'] })
+        .then((collected) => {
+        message.channel.send(`${collected.first().author} ✅ **صح عليك يا فنان**`); 
+
+        console.log(`[Typing] ${collected.first().author} typed the word.`);
+            let won = collected.first().author; 
+            points[won.id].points++;
+          })
+          .catch(collected => { 
+            message.channel.send(`:x: :stuck_out_tongue_winking_eye: **خطأ يا سبايك**`);
+          })
+        })
+    })
+}
+});
 
 
 

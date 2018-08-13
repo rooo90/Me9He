8fe2d74b7213cefe14fb402f65476557df3efb95
M7MD999 <40772560+M7MD999@users.noreply.github.com>
2018-08-13 22:57:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -113,29 +113,7 @@ if(message.content === adminprefix + "restart") {
   
   }); 
 
-const devs = ['419471939493429250' , '' , '' , ''];
-const adminprefix = "M";
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!devs.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + 'sp')) {
-    client.user.setGame(argresult);
-      message.channel.sendMessage(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'sw')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.sendMessage(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'sl')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.sendMessage(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/حب بلا حدود");
-      message.channel.sendMessage(`**✅   ${argresult}**`)
-  }
-  });
+
 
 
 

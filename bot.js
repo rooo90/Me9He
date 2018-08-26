34c6305263911cd595d62c21bd131fd6034305f8
M7MD999 <40772560+M7MD999@users.noreply.github.com>
2018-08-26 22:28:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -244,38 +244,48 @@ const embed = new Discord.RichEmbed()
 
 
 client.on('message', message => {
-  if (message.guild) {
- let embed = new Discord.RichEmbed()
-  let args = message.content.split(' ').slice(1).join(' ');
-if(message.content.split(' ')[0] == prefix + 'bc') {
-  if (!args[1]) {
-message.channel.send("**Mbc <message>**");
-return;
-}
-      message.guild.members.forEach(m => {
- if(!message.member.hasPermission('ADMINISTRATOR')) return;
-          var bc = new Discord.RichEmbed()
-          .setAuthor(message.author.username, message.author.avatarURL)
-          .addField(':diamond_shape_with_a_dot_inside:|السيرفر', `${message.guild.name}`, true)
-          .addField(':incoming_envelope:|المرسل', `${message.author.username}!${message.author.discriminator}`, true)
-          .addField(':page_facing_up:|الرسالة', args)
-          .setThumbnail(message.guild.iconURL)
-          .setColor('#00ff47')
-          m.send(`${m}`,{embed: bc});
-      });
-      const Himo = new Discord.RichEmbed()
-      .setAuthor(message.author.username, message.author.avatarURL)
-      .setTitle(':sleeping:|جاري التحميل')
-      .addBlankField(true)
-      .addField(':ballot_box_with_check:|ارسلت الى', message.guild.memberCount , true)
-      .addField(':pencil:|الرسالة', args)
-      .setColor('#00ff47')
-      message.channel.sendEmbed(embed);
-  }
-  } else {
-      return;
-  }
-});
+              if(!message.channel.guild) return;
+    var prefix = "M";
+    if(message.content.startsWith(prefix + 'bc')) {
+    if(!message.channel.guild) return message.channel.send('**الأمر بالسيرفرات بس**').then(m => m.delete(5000));
+  if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**ماعندك الصلاحية المطلوبة**' );
+    let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+    let copy = "M Games";
+    let request = `Requested By ${message.author.username}`;
+    if (!args) return message.reply('**ترسل برودكاست فاضي؟**');message.channel.send(`**متأكد؟** \` ${args}\``).then(msg => {
+    msg.react('✅')
+    .then(() => msg.react('❌'))
+    .then(() =>msg.react('✅'))
+
+    let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+    let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+
+    let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+    let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+    reaction1.on("collect", r => {
+    message.channel.send(`**☑ | Done ... The Broadcast Message Has Been Sent For ${message.guild.members.size} Members**`).then(m => m.delete(5000));
+    message.guild.members.forEach(m => {
+    var bc = new
+       Discord.RichEmbed()
+       .setColor('#00ff47')
+       .setTitle('**:incoming_envelope:رسالة:incoming_envelope:')
+       .addField('**:diamond_shape_with_a_dot_inside:السيرفر:diamond_shape_with_a_dot_inside:**', message.guild.name)
+       .addField('**:black_joker:المرسل:black_joker:**', message.author.username)
+       .addField(':page_facing_up:الرسالة:page_facing_up:', args)
+       .setThumbnail(message.author.avatarURL)
+       .setFooter(copy, client.user.avatarURL);
+    m.send({ embed: bc })
+    msg.delete();
+    })
+    })
+    reaction2.on("collect", r => {
+    message.channel.send(`**كنسل يا مدير**`).then(m => m.delete(5000));
+    msg.delete();
+    })
+    })
+    }
+    });
+
 
 client.on('message', message => {
     var prefix = "M"
@@ -425,64 +435,7 @@ if (!channel) return;
 channel.send({embed : embed});
 });
 
-client.on('guildMemberAdd',async member => {
-  const Canvas = require('canvas');
-  const jimp = require('jimp');
-  const w = ['./welcome_4.png'];
-        let Image = Canvas.Image,
-            canvas = new Canvas(800, 300),
-            ctx = canvas.getContext('2d');
-        ctx.patternQuality = 'bilinear';
-        ctx.filter = 'bilinear';
-        ctx.antialias = 'subpixel';
-        ctx.shadowColor = 'rgba(0, 0, 0, 0.4)';
-        ctx.shadowOffsetY = 2;
-        ctx.shadowBlur = 2;
-        ctx.stroke();
-        ctx.beginPath();
- 
-        fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
-            if (err) return console.log(err);
-            let BG = Canvas.Image;
-            let ground = new Image;
-            ground.src = Background;
-            ctx.drawImage(ground, 0, 0, 800, 300);
- 
-})
- 
-                let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".png" : member.user.displayAvatarURL;
-                jimp.read(url, (err, ava) => {
-                    if (err) return console.log(err);
-                    ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
-                 if (err) return console.log(err);
- 
-          ctx.font = '36px Arial';
-          ctx.fontSize = '72px';
-          ctx.fillStyle = "#ffffff";
-          ctx.textAlign = "center";
-          ctx.fillText(member.user.username, 545, 177);
-         
-          ctx.font = '16px Arial Bold';
-          ctx.fontSize = '72px';
-          ctx.fillStyle = "#ffffff";
-          ctx.textAlign = "center";
-          ctx.fillText(`${member.guild.memberCount} Members`, 580, 200);
-         
-          let Avatar = Canvas.Image;
-          let ava = new Avatar;
-          ava.src = buf;
-          ctx.beginPath();
-          ctx.arc(169.5, 148, 126.9, -100, Math.PI * 2, true);
-          ctx.closePath();
-          ctx.clip();
-          ctx.drawImage(ava, 36, 21, 260, 260);
-           
-var channel =member.guild.channels.find('name', 'welcome')
-          c.sendFile(canvas.toBuffer());
- 
-});
-});
-});
+
 
 
 
